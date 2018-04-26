9746e56a659fed802b3ce3c9467997eabd797b6d
Matt Miller <matt@megamattmiller.com>
2018-04-25 20:57:33 -0500

    Updated travis to check formatting. Formatted files.

--- lib/src/calculate_complimentary_color.dart
+++ lib/src/calculate_complimentary_color.dart
@@ -1,18 +1,15 @@
 import 'package:color/color.dart';
 
-class CalculateComplimentaryColor
-{
+class CalculateComplimentaryColor {
   CalculateComplimentaryColor();
 
   /// Calculates the complimentary color of a given hex value.
-  static HexColor fromHex(HexColor passedColor)
-  {
+  static HexColor fromHex(HexColor passedColor) {
     return fromHsl(passedColor.toHslColor()).toHexColor();
   }
 
   /// Calculates the complimentary color of a given hsl value.
-  static HslColor fromHsl(HslColor passedColor)
-  {
+  static HslColor fromHsl(HslColor passedColor) {
     num newH = passedColor.h + 180;
     if (newH > 360) newH -= 360;
     HslColor newHslColor = new HslColor(newH, passedColor.s, passedColor.l);
