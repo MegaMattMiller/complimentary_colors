4dd9b9364c5eda731254ded5c3b90c20262f8706
Matt Miller <matt@megamattmiller.com>
2018-04-25 21:04:21 -0500

    Added example file.

---
+++ example/example.dart
@@ -0,0 +1,16 @@
+import 'package:color/color.dart';
+import 'package:complimentary_colors/complimentary_colors.dart';
+
+class Example
+{
+  HexColor givenHexColor = new HexColor("FF0000");
+  HslColor givenHslColor = new HslColor(270, 100, 100);
+  Example()
+  {
+    //Get the complimentary color of "FF0000";
+    HexColor complimentaryHexColor = CalculateComplimentaryColor.fromHex(givenHexColor);
+
+    //Get the complimentary color of (270, 100, 100);
+    HslColor complimentaryHslColor = CalculateComplimentaryColor.fromHsl(givenHslColor);
+  }
+}
