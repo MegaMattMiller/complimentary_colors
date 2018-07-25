2f9f5c92c47f55834c3f8c2a336308e674c1a8a0
Matt Miller <matt@megamattmiller.com>
2018-07-25 18:26:09 -0500

    Updated example so it actually runs.

--- example/example.dart
+++ example/example.dart
@@ -1,21 +1,25 @@
 import 'package:color/color.dart';
 import 'package:complimentary_colors/complimentary_colors.dart';
 
-class Example {
+main() {
   HexColor givenHexColor = new HexColor("FF0000");
   HslColor givenHslColor = new HslColor(270, 100, 100);
   RgbColor givenRgbColor = new RgbColor(255, 132, 91);
-  Example() {
-    //Get the complimentary color of "FF0000";
-    HexColor complimentaryHexColor =
-        CalculateComplimentaryColor.fromHex(givenHexColor);
+  //Get the complimentary color of "FF0000";
+  HexColor complimentaryHexColor =
+      CalculateComplimentaryColor.fromHex(givenHexColor);
+  print("givenHexColor = $givenHexColor");
+  print("complimentaryHexColor = $complimentaryHexColor");
 
-    //Get the complimentary color of Hsl(270, 100, 100);
-    HslColor complimentaryHslColor =
-        CalculateComplimentaryColor.fromHsl(givenHslColor);
+  //Get the complimentary color of Hsl(270, 100, 100);
+  HslColor complimentaryHslColor =
+      CalculateComplimentaryColor.fromHsl(givenHslColor);
+  print("givenHslColor = $givenHslColor");
+  print("complimentaryHslColor = $complimentaryHslColor");
 
-    //Get the complimentary color of Rgb(255, 132, 91);
-    RgbColor complimentaryRgbColor =
-        CalculateComplimentaryColor.fromRgb(givenRgbColor);
-  }
+  //Get the complimentary color of Rgb(255, 132, 91);
+  RgbColor complimentaryRgbColor =
+      CalculateComplimentaryColor.fromRgb(givenRgbColor);
+  print("givenRgbColor = $givenRgbColor");
+  print("complimentaryRgbColor = $complimentaryRgbColor");
 }
